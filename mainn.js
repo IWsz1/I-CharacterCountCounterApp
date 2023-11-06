332efcda654bff2d3dd12b380d6da0dc9f627dc4
IWsz1 <133868773+IWsz1@users.noreply.github.com>
2023-11-06 21:21:47 +0900

    Add files via upload

---
+++ mainn.js
@@ -0,0 +1,46 @@
+let comment = document.getElementById("comment");
+let goal = document.getElementById("goal");
+let now = document.getElementById("now");
+let remaining = document.getElementById("remaining");
+
+comment.addEventListener("keyup",function(){
+  // innerHTMLでhtmlに書き換え　thisでこのイベントの関数（comment）　value.lengthで文字数
+  now.innerHTML = this.value.length;
+  let limit = goal.value - this.value.length;
+  remaining.innerHTML = limit;
+  if(limit > 10){
+    remaining.classList.remove("over")
+    remaining.classList.remove("nearover")
+    remaining.classList.add("noover")
+  }
+  if(limit <= 10){
+    remaining.classList.remove("noover")
+    remaining.classList.remove("over")
+    remaining.classList.add("nearover")
+  }
+  if(limit < 0){
+    remaining.classList.remove("noover")
+    remaining.classList.remove("nearover")
+    remaining.classList.add("over")
+  }
+})
+
+goal.addEventListener("keyup",function(){
+  let limit = goal.value - comment.value.length;
+  remaining.innerHTML = limit;
+  if(limit > 10){
+    remaining.classList.remove("over")
+    remaining.classList.remove("nearover")
+    remaining.classList.add("noover")
+  }
+  if(limit <= 10){
+    remaining.classList.remove("noover")
+    remaining.classList.remove("over")
+    remaining.classList.add("nearover")
+  }
+  if(limit < 0){
+    remaining.classList.remove("noover")
+    remaining.classList.remove("nearover")
+    remaining.classList.add("over")
+  }
+})
